3970990617d84e30fe80e00a6d24c09322b694e8
finntenzor <finntenzor@gmail.com>
2018-11-13 22:23:41 +0800

    编写登录/注册页面

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,46 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import axios from 'axios'
+export default {
+  name: 'App',
+  data() {
+    return {
+      info: null
+    }
+  },
+  methods: {
+    testLogin() {
+      const form = {
+        username: 'bill412',
+        password: '147896325'
+      }
+      axios.post('/api/v1/auth/login', form)
+      .then(response => {
+        this.info = response.data
+      })
+      .catch(err => {
+        console.error(err)
+      })
     }
   }
+}
+</script>
+
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  text-decoration: none;
+}
+#app {
+  height: 100vh;
+  overflow: hidden;
 }
 </style>
---
+++ src/views/Login/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="login">
+    <img class="login-logo" src="@/assets/logo.png">
+    <el-card>
+      <div class="login-form">
+        <el-form ref="form" :model="form" label-width="80px" label-position="top">
+          <el-form-item label="用户名">
+            <el-input v-model="form.username"></el-input>
+          </el-form-item>
+          <el-form-item label="密码">
+            <el-input v-model="form.password"></el-input>
+          </el-form-item>
+        </el-form>
+      </div>
+      <div class="login-action">
+        <el-button type="primary" @click="handleLogin()">登录</el-button>
+        <el-button size="small" @click="handleRegister()">注册</el-button>
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      form: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    handleRegister() {
+
+    },
+    handleLogin() {
+
+    }
+  }
+}
+</script>
+
+<style>
+.login-logo {
+  display: block;
+  width: 300px;
+  height: auto;
+  margin: 20px auto;
+}
+.login {
+  width: 420px;
+  margin: 0 auto;
+}
+.login-action {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
