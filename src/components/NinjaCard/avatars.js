dbad8ec36c7fbc2f1ac18e83754a16f67708de80
finntenzor <finntenzor@gmail.com>
2018-11-15 16:39:31 +0800

    添加忍者显示卡片

---
+++ src/components/NinjaCard/avatars.js
@@ -0,0 +1,21 @@
+import avatar_01 from '@/assets/ninjas/ninja_01.png'
+import avatar_02 from '@/assets/ninjas/ninja_02.png'
+import avatar_03 from '@/assets/ninjas/ninja_03.png'
+import avatar_04 from '@/assets/ninjas/ninja_04.png'
+import avatar_05 from '@/assets/ninjas/ninja_05.png'
+import avatar_06 from '@/assets/ninjas/ninja_06.png'
+import avatar_07 from '@/assets/ninjas/ninja_07.png'
+import avatar_08 from '@/assets/ninjas/ninja_08.png'
+import avatar_09 from '@/assets/ninjas/ninja_09.png'
+
+export default [
+  avatar_01,
+  avatar_02,
+  avatar_03,
+  avatar_04,
+  avatar_05,
+  avatar_06,
+  avatar_07,
+  avatar_08,
+  avatar_09,
+]
---
+++ src/components/NinjaCard/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <el-card class="ninja-card-container">
+    <div class="ninja-card">
+      <img class="ninja-card-avatar" :src="avatarSrc" alt="头像">
+      <div class="ninja-card-infos">
+        <ul class="ninja-card-attrs">
+          <li class="ninja-card-attr attr-atk">
+            <img class="ninja-card-attr-icon" src="@/assets/icons/sword.png" alt="攻击力">
+            <p class="ninja-card-attr-value">{{ ninja.atk }}</p>
+          </li>
+          <li class="ninja-card-attr attr-def">
+            <img class="ninja-card-attr-icon" src="@/assets/icons/shield.png" alt="防御力">
+            <p class="ninja-card-attr-value">{{ ninja.def }}</p>
+          </li>
+        </ul>
+        <el-button class="ninja-card-button" @click="handleFire">解雇</el-button>
+      </div>
+    </div>
+  </el-card>
+</template>
+
+<script>
+import avatars from './avatars'
+
+export default {
+  name: 'NinjaCard',
+  props: {
+    ninja: Object
+  },
+  data() {
+    return {
+      avatarSrc: ''
+    }
+  },
+  created() {
+    this.avatarSrc = avatars[parseInt(Math.random() * avatars.length)]
+  },
+  methods: {
+    handleFire() {
+
+    }
+  }
+}
+</script>
+
+<style>
+.ninja-card-container {
+  display: inline-flex;
+}
+.ninja-card {
+  display: flex;
+}
+.ninja-card-avatar {
+  display: block;
+  width: 150px;
+  height: 150px;
+  object-fit: contain;
+}
+
+.ninja-card-attr {
+  display: flex;
+  margin: 10px;
+  align-items: center;
+}
+
+.ninja-card-icon,
+.ninja-card-attr-icon {
+  display: block;
+  width: 40px;
+  height: 40px;
+  object-fit: contain;
+}
+
+.ninja-card-attr-value {
+  width: 2em;
+  margin: 0 20px;
+}
+</style>
