5b34270d4e7a97842f840ba6ce0ded02f4cb3376
finntenzor <finntenzor@gmail.com>
2018-11-16 21:19:14 +0800

    修正部分错误报告问题

--- src/mixins/errorReport.js
+++ src/mixins/errorReport.js
@@ -1,14 +1,14 @@
 export default {
   methods: {
     report(err) {
-      if (err) {
-        if (err.data && err.data.msg) {
-          this.$message.error(err.data.msg)
-        } else {
-          this.$message.error('服务器异常')
-        }
+      if (err && err.data && err.data.msg) {
+        this.$message.error(err.data.msg)
+      } else if (err && err.message) {
+        this.$message.error(err.message)
+      } else if (err && err.data && err.ret) {
+        this.$message.error('服务器错误')
       } else {
-        this.$message.error('网络异常')
+        this.$message.error('未知错误')
       }
     }
   }
