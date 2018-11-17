488b4d7d9d6e845471119784479c6396041dfc1a
finntenzor <finntenzor@gmail.com>
2018-11-17 16:31:53 +0800

    修复新用户信息计算错误的bug，并提供登出功能

--- src/components/UserInfo/index.vue
+++ src/components/UserInfo/index.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="user-info">
     <ninja-avatar class="user-info-avatar" :color="rarityColor" :id="userInfo.id"></ninja-avatar>
-    <p>欢迎您：{{ userInfo.username }}</p>
-    <p>账户余额：{{ userInfo.money }}</p>
-    <p>拥有忍者：{{ aliveNinjaList.length }}名</p>
-    <!-- 以下属性都是我瞎编的 -->
-    <p>当前战力：{{ power }}</p>
-    <p>总攻击：{{ totalAtk }}</p>
-    <p>总防御：{{ totalDef }}</p>
-    <!-- 凑字数 -->
-    <p>战胜BOSS：{{ killBoss }}个</p>
-    <p>最近战胜：{{ lastBoss }}</p>
+    <ul class="user-info-deatils">
+      <li>欢迎您：{{ userInfo.username }}</li>
+      <li>账户余额：{{ userInfo.money }}</li>
+      <li>拥有忍者：{{ aliveNinjaList.length }}名</li>
+      <!-- 以下属性都是我瞎编的 -->
+      <li>当前战力：{{ power }}</li>
+      <li>总攻击：{{ totalAtk }}</li>
+      <li>总防御：{{ totalDef }}</li>
+      <!-- 凑字数 -->
+      <li>战胜BOSS：{{ killBoss }}个</li>
+      <li>最近战胜：{{ lastBoss }}</li>
+    </ul>
+    <div class="user-info-actions">
+      <el-button type="primary" @click="logout()" plain>登出</el-button>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import { mapGetters, mapActions } from 'vuex'
 import NinjaAvatar from '@/components/NinjaAvatar'
 import rarityMixin from '@/mixins/rarity'
 
@@ -46,21 +51,26 @@ export default {
       }
     },
     totalAtk() {
-      return this.aliveNinjaList.map(ninja => ninja.atk).reduce((a, b) => a + b)
+      return this.aliveNinjaList.map(ninja => ninja.atk).reduce((a, b) => a + b, 0)
     },
     totalDef() {
-      return this.aliveNinjaList.map(ninja => ninja.def).reduce((a, b) => a + b)
+      return this.aliveNinjaList.map(ninja => ninja.def).reduce((a, b) => a + b, 0)
     },
     power() {
       return this.totalAtk + this.totalDef
     },
     killBoss() {
-      return this.bossList.map(boss => boss.last_kill > 0 ? 1 : 0).reduce((a, b) => a + b)
+      return this.bossList.map(boss => boss.last_kill > 0 ? 1 : 0).reduce((a, b) => a + b, 0)
     },
     lastBoss() {
       const list = this.bossList.filter(boss => boss.last_kill > 0).sort((a, b) => b.last_kill - a.last_kill)
       return list[0] ? list[0].name : '暂无'
     }
+  },
+  methods: {
+    ...mapActions({
+      logout: 'logout'
+    })
   }
 }
 </script>
@@ -68,11 +78,14 @@ export default {
 <style lang="scss">
 .user-info {
   padding: 10px;
-  p {
-    margin: 10px 0;
-  }
+}
+.user-info-deatils > li {
+  margin: 10px 0;
 }
 .user-info-avatar {
   margin: 0 auto;
+}
+.user-info-actions {
+  text-align: center;
 }
 </style>
